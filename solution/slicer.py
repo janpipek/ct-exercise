43501ebaac3e90890081edd09cae478a3f33d46b
Jan Pipek <jan.pipek@gmail.com>
2023-05-03 10:21:29 +0200

    Slicer solution

--- solution/slicer.py
+++ solution/slicer.py
@@ -1,11 +1,11 @@
-# Create a script using typer that will find the x, y and z slices with the highest
-# fraction of gray mass and plot them into separate files
+from pathlib import Path
+from typing import Literal
 
+import matplotlib.pyplot as plt
 import typer
 import xarray
-from pathlib import Path
-import matplotlib.pyplot as plt
-
+from matplotlib.figure import Figure
+from typing_extensions import Annotated
 
 DENSITIES = {
     "air": (-1000, -900),
@@ -15,54 +15,66 @@ DENSITIES = {
     "grey": (30, 50),
     "soft": (50, 300),
     "bone": (300, 1900),
-    "heavy": (1900, 30000)
+    "heavy": (1900, 30000),
 }
 
 
-def find_max_idx(data, axis):
+def find_max_idx(data: xarray.DataArray, axis: Literal["x", "y", "z"]) -> float:
+    """Find the index label with highest bone content in the slice."""
     is_bone = (data > DENSITIES["bone"][0]) & (data < DENSITIES["bone"][1])
-    dims = list({"x","y", "z"} - {axis})
+    dims = list({"x", "y", "z"} - {axis})
     return is_bone.sum(dims).idxmax()
 
 
-def physio_plot2(data, axis, value, ax):
-    slice = data.sel(**{axis: value})
-    slice.plot(cmap="gist_gray", vmin=-100, vmax=1000, ax=ax)
-
-
-def plot_three_planes(data, **kwargs):
+def draw_slices(data: xarray.DataArray, **kwargs) -> Figure:
+    """Draw three slices horizontally arranged."""
     fig, ax = plt.subplots(1, 3, figsize=(12, 4))
     for i, axis in enumerate(["x", "y", "z"]):
         idx = find_max_idx(data, axis)
-        slice = data.sel(**{axis: idx})
+        slice = data.sel(**{axis: idx}, method="nearest")
         slice.plot(**kwargs, ax=ax[i])
     return fig
 
 
 def main(
     # Subtask: Define the input_path and output_path arguments
-    input_path: Path,
-    output_path: Path = Path("plot.png"),
+    input_path: Annotated[Path, typer.Argument(help="File to read from")],
+    output_path: Annotated[Path, typer.Argument(help="File to write to")] = Path(
+        "plot.png"
+    ),
     # Subtask: Define arguments you want the user to be able to specify
-    vmin: float = -100,
-    vmax: float = 1000,
-    cmap: str = "gist_gray",
-    segmented: bool = False,
+    vmin: Annotated[
+        float, typer.Option(help="Max HU value to be shown as black")
+    ] = -100,
+    vmax: Annotated[
+        float, typer.Option(help="Min HU value to be shown as white")
+    ] = 1000,
+    cmap: Annotated[str, typer.Option(help="Colormap to use")] = "gist_gray",
+    segmented: Annotated[
+        bool,
+        typer.Option(help="If true, different tissues will be having solid colors"),
+    ] = False,
 ):
+    """Draw three plots with sagittal, coronal and axial slices of CT images.
+
+    It puts them all next to each other and stores them into a file.
+    """
+
     # Load the data
     data = xarray.load_dataarray(input_path) - 1000
 
-    # Bonus: Use the levels argument to separate the various tissue types
+    # Use the `levels`` argument to separate the various tissue types
     if segmented:
         levels = [
-            DENSITIES[k][0] for k in ["air", "fat", "fluid", "white", "grey", "soft", "bone"]
+            DENSITIES[k][0]
+            for k in ["air", "fat", "fluid", "white", "grey", "soft", "bone"]
         ] + [DENSITIES["bone"][1]]
     else:
         levels = None
 
-    fig = plot_three_planes(data, vmin=vmin, vmax=vmax, cmap=cmap, levels=levels)
+    fig = draw_slices(data, vmin=vmin, vmax=vmax, cmap=cmap, levels=levels)
 
-    # Subtask: Save the figure 
+    # Save the figure
     fig.tight_layout()
     fig.savefig(output_path)
 
